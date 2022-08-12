e7ce20658665866c9a6ba7e1d7755b639e32f623
SIArifBD <si.arif.cse@gmail.com>
2022-08-12 18:57:22 +0600

    arif

--- index.js
+++ index.js
@@ -3,13 +3,14 @@ const cors = require('cors');
 const app = express();
 require('dotenv').config();
 const jwt = require('jsonwebtoken');
-const { MongoClient, ServerApiVersion, ObjectId,} = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId, } = require('mongodb');
 const port = process.env.PORT || 5000;
 
 //midleware
 app.use(cors());
 app.use(express.json());
 
+//
 // mongoDB 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.zv0zn.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
