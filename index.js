c6666821d3159af2078702f2e7052e691abc5110
rabiul1399 <www.rabiulislam3808@gamil.com>
2022-08-12 15:36:24 +0600

    no change

--- index.js
+++ index.js
@@ -126,7 +126,7 @@ async function run() {
         // app.get('/article/:id', async (req, res) => {
         //     const id = req.params.id;
         //     const query = { _id: ObjectId(id) };
-        //     const result = await articleCollection.findOne(query);
+        //     const result = await articleCollection.findOne({ _id: ObjectId(id) });
         //     res.send(result);
         // });
 
